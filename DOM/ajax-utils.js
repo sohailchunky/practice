5004abcd48014478bdb643c303170d0833ce7ac3
sohailchunky <sohail.shaikh78647@gmail.com>
2020-10-07 12:14:39 -0700

    files added

--- DOM/ajax-utils.js
+++ DOM/ajax-utils.js
@@ -1,1 +1,22 @@
-hello world
+(function (global) {
+	var ajaxUtils = {};
+
+	function getRequestObject()
+	{
+		if(window.XMLHttpRequest){
+			return(new XMLHttpRequest());
+		}
+		else if (window.ActiveXObject)
+		{
+			return(new ActiveXObject("Microsoft.XMLHTTP"));		
+		}
+		else {
+			global.alert("Ajax is not supported!");
+			return(null);
+		}
+		}
+
+		ajaxUtils.send
+	}
+
+})
